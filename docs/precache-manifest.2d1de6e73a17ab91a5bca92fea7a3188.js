0544c4be12670c6b6c098baaf7ebb24ae89ef9bf
Yilin Tsai <ylt1215@uw.edu>
2020-02-16 18:50:55 -0800

    deploy

---
+++ docs/precache-manifest.2d1de6e73a17ab91a5bca92fea7a3188.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b47c2f67854123b078271bae9260dbd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c19352b6701b942cebe9",
+    "url": "/portfolio/static/css/main.a81ffad9.chunk.css"
+  },
+  {
+    "revision": "f686e99a9b6a6c927b70",
+    "url": "/portfolio/static/js/2.4afbc96e.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/portfolio/static/js/2.4afbc96e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19352b6701b942cebe9",
+    "url": "/portfolio/static/js/main.003fa67c.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "4e71b4343f66429a7474f17f9a5b209a",
+    "url": "/portfolio/static/media/header.4e71b434.jpg"
+  },
+  {
+    "revision": "fa127cbfda9bbc1670e7c1ed60a34129",
+    "url": "/portfolio/static/media/profilepic.fa127cbf.jpg"
+  }
+]);
